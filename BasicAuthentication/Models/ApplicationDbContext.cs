4f826f463945ad44187da0b659cb899933339608
Sara <sara@indeterminate.net>
2018-04-28 13:39:04 -0700

    add migration to add item table

--- BasicAuthentication/Models/ApplicationDbContext.cs
+++ BasicAuthentication/Models/ApplicationDbContext.cs
@@ -9,5 +9,11 @@ namespace BasicAuthentication.Models
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+        public DbSet<Item> Items { get; set; }
     }
 }
